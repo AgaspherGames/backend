f1cafe3ed7cef2ea417ca2c33345aa0908ba5976
Agaspher <100010896+AgaspherGames@users.noreply.github.com>
2023-01-28 12:01:55 +0600

    Add files via upload

--- sitea/calc.php
+++ sitea/calc.php
@@ -1,5 +1,31 @@
+<?php
+if (isset($_POST['num1']) && isset($_POST['operator']) && isset($_POST['num2'])) {
+  $num1 = $_POST['num1'];
+  $operator = $_POST['operator'];
+  $num2 = $_POST['num2'];
+  switch ($operator) {
+    case '+':
+      $result = $num1 + $num2;
+      break;
+    case '-':
+      $result = $num1 - $num2;
+      break;
+    case '/':
+      $num2 != 0 ? $result =  ($num1 / $num2) : $result = ('НЕЛЬЗЯ ДЕЛИТЬ НА НОЛЬ!!!!');
+      break;
+    case '*':
+      $result = $num1 * $num2;
+      break;
+    default:
+      $result = 'Неверный оператор';
+      break;
+  }
+  echo "<h4>Результат: $result</h4>";
+}
+?>
+
 <!-- Область основного контента -->
-<form action=''>
+<form action='' method="post">
   <label>Число 1:</label>
   <br />
   <input name='num1' type='text' />
